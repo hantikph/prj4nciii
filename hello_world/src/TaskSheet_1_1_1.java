75ef156f6c50315038b6a3272be70cda7f139acf
Charles A. Mijares <37148178+hantikph@users.noreply.github.com>
2025-06-17 16:29:29 +0800

    add java TaskSheet_1_1_1

---
+++ hello_world/src/TaskSheet_1_1_1.java
@@ -0,0 +1,15 @@
+
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+
+public class TaskSheet_1_1_1 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+
+}
