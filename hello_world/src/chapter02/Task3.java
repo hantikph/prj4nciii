4ad7e78924fb8227e2c6d44c2fe5051c3f77304e
Charles A. Mijares <37148178+hantikph@users.noreply.github.com>
2025-07-01 16:04:40 +0800

    update code Task3

--- hello_world/src/chapter02/Task3.java
+++ hello_world/src/chapter02/Task3.java
@@ -1,4 +1,18 @@
 package chapter02;
 
 public class Task3 {
+    public static void main(String[] args) {
+        String a = new String("Wow");
+        String b = "Wow";
+        String c = a;
+        String d = c + "!";
+
+        boolean b1 = a.equals(b);
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = c.equals(a);
+
+        if(b1 && b2 && b3){
+            System.out.println("Success!");
+        }
+    }
 }
