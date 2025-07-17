048135fa37aa13971e3837237b3725ec6cc38509
Charles A. Mijares <37148178+hantikph@users.noreply.github.com>
2025-07-17 17:06:27 +0800

    added code Task14.java

---
+++ hello_world/src/chapter05/Task14.java
@@ -0,0 +1,33 @@
+package chapter05;
+
+public class Task14 {
+    public static void main(String[] args) {
+        B b = new B();
+        b.abstractMethod();
+        b.concreteMethod();
+        C c = new C();
+        c.abstractMethod();
+        c.concreteMethod();
+    }
+}
+
+abstract class abstractClass {
+    abstract void abstractMethod();
+    void concreteMethod() {
+        System.out.println("This is a concrete method.");
+    }
+}
+
+class B extends abstractClass {
+    void abstractMethod() {
+        System.out.println("B's implementation of abstractMethod.");
+    }
+}
+
+// create a subclass of the abstract class and implement the abstract method
+class C extends abstractClass {
+    // your code goes here
+    void abstractMethod() {
+        System.out.println("C's implementation of abstractMethod.");
+    }
+}
